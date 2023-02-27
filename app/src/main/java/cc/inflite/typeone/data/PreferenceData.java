62b5cf2b33a7a608c10b38e9b86ab59ff56b8f93
Marcin Juraszek <mail@marcinjuraszek.com>
2023-02-26 22:10:10 -0800

    Allow for api token to be set

--- app/src/main/java/cc/inflite/typeone/data/PreferenceData.java
+++ app/src/main/java/cc/inflite/typeone/data/PreferenceData.java
@@ -3,15 +3,20 @@ package cc.inflite.typeone.data;
 public class PreferenceData {
 
     private final String serverAddress;
+    private final String apiSecret;
     private final int updateFrequency;
 
-    public PreferenceData(String serverAddress, int updateFrequency) {
+    public PreferenceData(String serverAddress, int updateFrequency, String apiSecret) {
         this.serverAddress = serverAddress;
         this.updateFrequency = updateFrequency;
+        this.apiSecret = apiSecret;
     }
 
     public String getServerAddress() {
         return serverAddress;
+    }
+    public String getApiSecret() {
+        return apiSecret;
     }
 
     public int getUpdateFrequency() {
